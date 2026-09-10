44cb16831051d39f9257c1cda29762143f71fe70
saksham704 <sb031428@gmail.com>
2026-09-10 19:34:35 +0530

    Add TrappingRainwater.java

---
+++ BuyandSellStocks.java
@@ -0,0 +1,20 @@
+public class BuyandSellStocks {
+    public static int maxprofit(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i=0; i<prices.length; i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+            } else {
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String args[]){
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("The maximum profit is: " + maxprofit(prices));
+    }
+    
+}
---
+++ PascalTriangle.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++){
+
+            List<Integer> row = new ArrayList<>();
+
+            for(int j = 0; j <= i; j++){
+
+                if(j == 0 || j == i){
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+                }
+
+            }
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+    public static void main(String[] args) {
+        PascalTriangle obj = new PascalTriangle();
+        int numRows = 5;
+        List<List<Integer>> result = obj.generate(numRows);
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+}
